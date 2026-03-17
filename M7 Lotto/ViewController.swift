a6eb34aa62409074c2a5ea432988f6152dc696d9
BCooperF <bradley_faesy@mymail.eku.edu>
2026-03-17 12:04:27 -0400

    Initial Commit

---
+++ M7 Lotto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M7 Lotto
+//
+//  Created by Faesy, Bradley C. on 3/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
